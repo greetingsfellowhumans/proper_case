52214f083c3755479e088d9803aa6add0028f6d5
greetingsfellowhumans <accounts@aaronjprice.com>
2026-07-05 21:36:30 -0700

    fix duplicate @doc for snake_case/1

--- lib/proper_case.ex
+++ lib/proper_case.ex
@@ -87,7 +87,7 @@ defmodule ProperCase do
   end
 
   @doc """
-  Converts an to `snake_case` string`
+  Converts an atom or string to a `snake_case` string`
   """
   def snake_case(val) when is_atom(val) do
     val
@@ -99,10 +99,7 @@ defmodule ProperCase do
     val
   end
 
-  @doc """
-  Converts a string to `snake_case`
-  """
-  def snake_case(val) do
+  def snake_case(val) when is_binary(val) do
     val
     |> String.replace(" ", "")
     |> Macro.underscore()
